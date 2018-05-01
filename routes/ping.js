ce3d096eaa2cc0acb7bb6de9d9ee51e221f18bfb
anoopelias <anoopelias@gmail.com>
2018-05-01 19:39:25 +0530

    Removed express / heroku related files
    Signed-off-by: anoopelias <anoopelias@gmail.com>

--- routes/ping.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(req, res) {
-    const name = req.query.name || '<Unknown>';
-    console.log('Responding to ping from', name);
-    res.send('Response to ping for ' + name + ' at ' + new Date());
-};
